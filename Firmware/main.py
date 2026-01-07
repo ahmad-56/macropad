a57dc985f49e27d1f25a36eb26d4909d400741fa
ahmad-56 <ahmad392.hassan@gmail.com>
2026-01-07 22:42:08 +0500

    Update main.py

--- Firmware/main.py
+++ Firmware/main.py
@@ -1,35 +1,59 @@
-# You import all the IOs of your board
 import board
 
-# These are imports from the kmk library
 from kmk.kmk_keyboard import KMKKeyboard
-from kmk.scanners.keypad import KeysScanner
 from kmk.keys import KC
-from kmk.modules.macros import Press, Release, Tap, Macros
+from kmk.extensions.media_keys import MediaKeys
+from kmk.extensions.rotary_encoder import RotaryEncoder
+from kmk.modules.rgb import RGB
+from kmk.extensions.display import OLED
 
-# This is the main instance of your keyboard
 keyboard = KMKKeyboard()
 
-# Add the macro extension
-macros = Macros()
-keyboard.modules.append(macros)
+keyboard.col_pins = (board.GP26, board.GP27, board.GP28)   # Example: 3 columns
+keyboard.row_pins = (board.GP4, board.GP2, board.GP1)   # Example: 3 rows
+keyboard.diode_orientation = "COL2ROW"                 # Adjust if needed
 
-# Define your pins here!
-PINS = [board.D3, board.D4, board.D2, board.D1]
-
-# Tell kmk we are not using a key matrix
-keyboard.matrix = KeysScanner(
-    pins=PINS,
-    value_when_pressed=False,
-)
-
-# Here you define the buttons corresponding to the pins
-# Look here for keycodes: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/keycodes.md
-# And here for macros: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/macros.md
 keyboard.keymap = [
-    [KC.A, KC.DELETE, KC.MACRO("Hello world!"), KC.Macro(Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD)),]
+    [
+        KC.DEL, KC.PRINT_SCREEN, KC.NO,  # Row 0
+        KC.LGUI, KC.UP, KC.KC.with_mods(KC.V, KC.MOD_LGUI), #WIN+V    # Row 1
+        KC.LEFT, KC.DOWN, KC.RIGHT     # Row 2
+    ]
 ]
 
-# Start kmk!
-if __name__ == '__main__':
+encoder = RotaryEncoder(
+    encoder_pins=(board.GP29, board.GP3),
+    ccw=KC.VOLD,                          
+    cw=KC.VOLU,                           
+    button=KC.MUTE
+)
+keyboard.extended = [encoder]
+
+keyboard.modules.append(
+    RGB(
+        pin=board.GP0,
+        num_leds=8,
+        brightness=0.4,
+        auto_write=True
+    )
+)
+
+from kmk.extensions.display import OLED
+
+i2c = board.I2C(scl=board.GP7, sda=board.GP6)
+
+keyboard.extensions.append(
+    OLED(
+        width=128,
+        height=32,
+        i2c_bus=i2c,
+        flip=False,
+        scroll=False
+    )
+)
+
+
+keyboard.debug_enabled = True
+
+if __name__ == "__main__":
     keyboard.go()
